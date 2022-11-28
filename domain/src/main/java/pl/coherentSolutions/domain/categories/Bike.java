48331cdce04c04cccc66b28e7792affe9b2da265
KonstantinLevchuk <KonstantinLevchuk@coherentsolutions.com>
2022-11-28 16:36:07 +0100

    added category, product, store, storeApp

---
+++ domain/src/main/java/pl/coherentSolutions/domain/categories/Bike.java
@@ -0,0 +1,10 @@
+package pl.coherentSolutions.domain.categories;
+
+import pl.coherentSolutions.domain.Category;
+
+public class Bike extends Category {
+
+    public Bike() {
+        super("Bike");
+    }
+}
