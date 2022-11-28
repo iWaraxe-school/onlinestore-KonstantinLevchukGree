d7ec8a728bd3afcbc3fafcef8e194afc22d99adf
KonstantinLevchuk <KonstantinLevchuk@coherentsolutions.com>
2022-11-28 16:39:09 +0100

    added store

---
+++ store/src/main/java/pl/coherentSolutions/store/Store.java
@@ -0,0 +1,18 @@
+package pl.coherentSolutions.store;
+
+import pl.coherentSolutions.domain.Category;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Store {
+    private List<Category> categoryList;
+
+    public Store() {
+        categoryList = new ArrayList<>();
+    }
+
+    public List<Category> getCategoryList() {
+        return categoryList;
+    }
+}
