042b092711c6c832e174cdae5f589452b238c73c
KonstantinLevchuk <KonstantinLevchuk@coherentsolutions.com>
2022-12-03 19:36:33 +0100

    added interaction

--- consoleApp/src/main/java/pl/coherentSolutions/consoleApp/StoreApp.java
+++ consoleApp/src/main/java/pl/coherentSolutions/consoleApp/StoreApp.java
@@ -2,27 +2,41 @@ package pl.coherentSolutions.consoleApp;
 
 import pl.coherentSolutions.products.Product;
 import pl.coherentSolutions.store.Store;
-import pl.coherentSolutions.store.constant.SortKey;
 import pl.coherentSolutions.store.utils.SortHelper;
 import pl.coherentSolutions.store.utils.StoreHelper;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class StoreApp {
     public static void main(String[] args) {
         Store store = new Store();
         StoreHelper helper = new StoreHelper(store);
+        SortHelper sortHelper = new SortHelper(store);
         helper.fillStoreRandomly();
 
-        List<Product> unSortedList = store.getAllProducts();
-        unSortedList.forEach(System.out::println);
-
-        SortHelper sortHelper = new SortHelper(store);
-        List<Product> sorted = sortHelper.sortByXml(SortKey.NAME.getKey());
-        sorted.forEach(System.out::println);
-
-        System.out.println("--------------------------------------------------------");
-        List<Product> topExpensiveProduct = sortHelper.topExpensiveProduct(5);
-        topExpensiveProduct.forEach(System.out::println);
+        System.out.println("Welcome to online store\nCommands:\nsort\ntop\nquit\nEnter command:");
+        Boolean flag = true;
+        while (flag) {
+            Scanner scanner = new Scanner(System.in);
+            String command = scanner.nextLine();
+            switch (command) {
+                case "sort":
+                    List<Product> sorted = sortHelper.sortByXml("name");
+                    sorted.forEach(System.out::println);
+                    scanner.close();
+                    flag = false;
+                    break;
+                case "top":
+                    List<Product> sortedTop = sortHelper.topExpensiveProduct(5);
+                    sortedTop.forEach(System.out::println);
+                    flag = false;
+                case "quit":
+                    flag = false;
+                    break;
+                default:
+                    System.out.println("Command is not supported. Enter commands: sort or top or quit");
+            }
+        }
     }
 }
